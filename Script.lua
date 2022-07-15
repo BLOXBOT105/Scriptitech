29bf19a01133668e2d7017d28334d2100c9d6a85
SamFromPirona <68560474+BLOXBOT105@users.noreply.github.com>
2022-07-14 22:42:41 -0400

    v1.1

--- Script.lua
+++ Script.lua
@@ -4,10 +4,15 @@
 -- Instances:
 
 local Scriptitect = Instance.new("ScreenGui")
-local Frame = Instance.new("Frame")
+local SelectGame = Instance.new("Frame")
 local TopBar = Instance.new("TextLabel")
 local UICorner = Instance.new("UICorner")
 local UICorner_2 = Instance.new("UICorner")
+local GameButtons = Instance.new("Folder")
+local None = Instance.new("TextButton")
+local UICorner_3 = Instance.new("UICorner")
+local PlaneCrazy = Instance.new("TextButton")
+local UICorner_4 = Instance.new("UICorner")
 
 --Properties:
 
@@ -15,14 +20,15 @@ Scriptitect.Name = "Scriptitect"
 Scriptitect.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
 Scriptitect.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
-Frame.Parent = Scriptitect
-Frame.BackgroundColor3 = Color3.fromRGB(69, 69, 69)
-Frame.Position = UDim2.new(0.303529412, 0, 0.315789461, 0)
-Frame.Size = UDim2.new(0, 500, 0, 350)
+SelectGame.Name = "SelectGame"
+SelectGame.Parent = Scriptitect
+SelectGame.BackgroundColor3 = Color3.fromRGB(69, 69, 69)
+SelectGame.Position = UDim2.new(0.303529412, 0, 0.315789461, 0)
+SelectGame.Size = UDim2.new(0, 500, 0, 350)
 
 TopBar.Name = "TopBar"
-TopBar.Parent = Frame
-TopBar.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
+TopBar.Parent = SelectGame
+TopBar.BackgroundColor3 = Color3.fromRGB(35, 35, 35)
 TopBar.Position = UDim2.new(0, 0, -0.0829999968, 0)
 TopBar.Size = UDim2.new(0, 500, 0, 50)
 TopBar.Font = Enum.Font.SourceSans
@@ -36,4 +42,52 @@ UICorner.CornerRadius = UDim.new(0, 12)
 UICorner.Parent = TopBar
 
 UICorner_2.CornerRadius = UDim.new(0, 12)
-UICorner_2.Parent = Frame
+UICorner_2.Parent = SelectGame
+
+GameButtons.Name = "GameButtons"
+GameButtons.Parent = SelectGame
+
+None.Name = "None"
+None.Parent = GameButtons
+None.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
+None.Position = UDim2.new(0.0219999999, 0, 0.0879999995, 0)
+None.Size = UDim2.new(0, 200, 0, 50)
+None.Font = Enum.Font.SourceSans
+None.Text = "Error!"
+None.TextColor3 = Color3.fromRGB(200, 0, 0)
+None.TextScaled = true
+None.TextSize = 14.000
+None.TextWrapped = true
+
+UICorner_3.CornerRadius = UDim.new(0, 12)
+UICorner_3.Parent = None
+
+PlaneCrazy.Name = "PlaneCrazy"
+PlaneCrazy.Parent = GameButtons
+PlaneCrazy.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
+PlaneCrazy.Position = UDim2.new(0.0214117467, 0, 0.257249892, 0)
+PlaneCrazy.Size = UDim2.new(0, 200, 0, 50)
+PlaneCrazy.Font = Enum.Font.SourceSans
+PlaneCrazy.Text = "Error!"
+PlaneCrazy.TextColor3 = Color3.fromRGB(200, 0, 0)
+PlaneCrazy.TextScaled = true
+PlaneCrazy.TextSize = 14.000
+PlaneCrazy.TextWrapped = true
+
+UICorner_4.CornerRadius = UDim.new(0, 12)
+UICorner_4.Parent = PlaneCrazy
+
+-- Scripts:
+
+local function XDNONWD_fake_script() -- None.ButtonScript 
+	local script = Instance.new('LocalScript', None)
+
+	script.Parent.Text = script.Parent.Name
+end
+coroutine.wrap(XDNONWD_fake_script)()
+local function GOVGVU_fake_script() -- PlaneCrazy.ButtonScript 
+	local script = Instance.new('LocalScript', PlaneCrazy)
+
+	script.Parent.Text = script.Parent.Name
+end
+coroutine.wrap(GOVGVU_fake_script)()
